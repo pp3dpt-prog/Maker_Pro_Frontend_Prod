1fd172e4a927fd34da1b58b11ed492cc93cfd957
PP3D-PT <PP3D.PT@gmail.com>
2026-05-13 23:29:16 +0100

    pricing

--- app/customizador/PageInner.tsx
+++ app/customizador/PageInner.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import { useEffect, useState } from 'react';
+import Link from 'next/link';
 import { useSearchParams, useRouter } from 'next/navigation';
 import { supabase } from '@/lib/supabaseClient';
 import GeneratedEditor from '@/components/GeneratedEditor';
